ad2ed232e8479f9ffa543089e698877e87e8d3c9
imi.fc <imfuert89@gmail.com>
2022-06-20 17:44:47 +0200

    newbranch

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,6 +1,27 @@
 import React from 'react'
 
 function Product({name,price,description,image,id}) {
+  const [products, setProducts] = useState([]);
+    useEffect(() => {
+        async function getElement() {
+
+            const res = await axios.get("http://localhost:8080/products/list");
+            setProducts(res.data.product)
+        }
+        getElement();
+
+
+    }, [])
+
+    const element = products.map((p) => <ProductsMin
+        name={p.name}
+        price={p.price}
+        description={p.description}
+        image={p.image}
+        id={p.id} />
+    );
+ 
+ 
   return (
     <div> 
 
@@ -9,6 +30,9 @@ function Product({name,price,description,image,id}) {
         <div>{description}</div>
         <div>{image}</div>
         <button>{id}</button>
+        <div>
+
+        </div>
 
     </div>
   )
