992e824d9e04ff2f1377fcb673771fca5783ed15
Xavimat <xaviermatoses@gmail.com>
2022-06-23 12:08:55 +0200

    redirect in 'profile' if not logged in

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,22 +1,30 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { UserContext } from "../../context/UserContext/UserState";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function Profile() {
   const { user, logout } = useContext(UserContext);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!user) {
+      return navigate("/login");
+    }
+  }, [user]);
+
 
   const logOutUser = () => {
     logout();
   };
+
   return (
     <div>
-      <div>{user.avatar}</div>
+      <div>{user?.avatar}</div>
       <div>
-        {user.username}, {user.firstName} {user.lastName}
+        {user?.username}, {user?.firstName} {user?.lastName}
       </div>
-      <div>{user.email}</div>
-      <div>{user.gender}</div>
-
+      <div>{user?.email}</div>
+      <div>{user?.gender}</div>
       <Link onClick={logOutUser} to={"/"}>
         LogOut
       </Link>
