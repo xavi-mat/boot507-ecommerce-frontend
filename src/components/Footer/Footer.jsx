d98639fff7606393c61dcc4ecdd17f17f8264005
Xavimat <xaviermatoses@gmail.com>
2022-06-27 16:07:53 +0200

    Footer with more space

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,26 +1,21 @@
-import React from 'react';
-
-
 function Footer() {
   return (
-    
-    <footer
-      style={{
-        textAlign: 'center',
-        backgroundColor: 'white',
-        borderTop: '1px solid #e8e8e8',
-        position: 'fixed',
-        left: 0,
-        bottom: 0,
-        width: '100%',
-
-        
-
-      }}
-    >
-      Ant Design ©2018 Created by Ant UED
-    </footer>
-  
+    <>
+      <div><h3 style={{ color: "#00000000" }}>Blank Space</h3></div>
+      <footer
+        style={{
+          textAlign: 'center',
+          backgroundColor: 'white',
+          borderTop: '1px solid #e8e8e8',
+          position: 'fixed',
+          left: 0,
+          bottom: 0,
+          width: '100%',
+        }}
+      >
+        SoulWear - © 2018 by Imanol & Xavi
+      </footer>
+    </>
   )
 }
 
