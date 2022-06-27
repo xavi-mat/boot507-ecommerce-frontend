7ae5c6195611ea2c666eb0ed5177f1af93734847
imi.fc <imfuert89@gmail.com>
2022-06-27 17:55:56 +0200

    cambioshome

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,45 +1,40 @@
 import React from 'react';
-import { Col, Row, Carousel } from 'antd';
+import { Carousel } from 'antd';
 import logo from '../../assets/logotipo.png';
 import titulo from '../../assets/titulo.png'
-import './Home.css'
 
 
-const contentStyle = {
-  height: '40%',
-  lineHeight: '50px',
-  textAlign: 'center',
 
-};
 
 
 function Home() {
   return (
-    <div>
+    <div className='home'>
+
       
+        <img className='logo' src={logo} alt="" />
       
 
-    <Row>
-      <Col span={6}><img style={contentStyle}  src={logo} alt="" /></Col>
-      <Col span={2}></Col>
-      <Col span={10}>
-      <img className='titulo'  src={titulo} alt="" />
+      <div className='container'>
+        <img className='titulo' src={titulo} alt="" />
 
-      <Carousel autoplay>
-    
-        <img style={contentStyle} src="https://images.pexels.com/photos/8794858/pexels-photo-8794858.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
-        <img style={contentStyle} src="https://images.pexels.com/photos/10806583/pexels-photo-10806583.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
-        <img style={contentStyle} src="https://images.pexels.com/photos/532220/pexels-photo-532220.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
-        <img style={contentStyle} src="https://images.pexels.com/photos/1043473/pexels-photo-1043473.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
-    
-      </Carousel>
-      </Col>
-      <Col span={2}></Col>
-      <Col span={6}>
-        
-      </Col>
-    </Row>
-    
+
+        <div className='paco'>
+
+        <Carousel className='carousel' autoplay >
+
+          <img  src="https://images.pexels.com/photos/8794858/pexels-photo-8794858.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+          <img  src="https://images.pexels.com/photos/10806583/pexels-photo-10806583.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+          <img  src="https://images.pexels.com/photos/532220/pexels-photo-532220.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+          <img  src="https://images.pexels.com/photos/1043473/pexels-photo-1043473.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+
+        </Carousel>
+        </div>
+      </div>
+
+
+
+
     </div>
   )
 }
