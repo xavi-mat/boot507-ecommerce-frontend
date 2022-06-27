daeca2016570298942077dbf88ba6a15f800abaa
imi.fc <imfuert89@gmail.com>
2022-06-27 13:34:59 +0200

    cambios en home estilo

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,31 +1,30 @@
 import React from 'react';
 import { Col, Row, Carousel } from 'antd';
+import logo from '../../assets/logotipo.png';
+import titulo from '../../assets/titulo.png'
+import './Home.css'
 
 
-const contentStyle: React.CSSProperties = {
+const contentStyle = {
   height: '40%',
-  color: '#fff',
   lineHeight: '50px',
   textAlign: 'center',
-  background: '#364d79',
-  
+
 };
 
 
 function Home() {
   return (
     <div>
-      <Row>
-      <Col span={6}></Col>
-      <Col span={12}>
-        <img src="../assets/logotipo.png" alt="" />
-      </Col>
-      <Col span={6}></Col>
-    </Row>
+      
+      
 
     <Row>
-      <Col span={6}></Col>
-      <Col span={12}>
+      <Col span={6}><img style={contentStyle}  src={logo} alt="" /></Col>
+      <Col span={2}></Col>
+      <Col span={10}>
+      <img className='titulo'  src={titulo} alt="" />
+
       <Carousel autoplay>
     
         <img style={contentStyle} src="https://images.pexels.com/photos/8794858/pexels-photo-8794858.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
@@ -35,7 +34,10 @@ function Home() {
     
       </Carousel>
       </Col>
-      <Col span={6}></Col>
+      <Col span={2}></Col>
+      <Col span={6}>
+        
+      </Col>
     </Row>
     
     </div>
